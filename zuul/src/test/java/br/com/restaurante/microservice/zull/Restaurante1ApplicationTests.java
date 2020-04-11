bd5bfc9e76952bcc488fbc47b91c6f751cf17550
Renato Borges <renatoborges0501@gmail.com>
2020-04-11 18:16:21 -0300

    configurando api gateway com zuul .

---
+++ zuul/src/test/java/br/com/restaurante/microservice/zull/Restaurante1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restaurante.microservice.zull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Restaurante1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
